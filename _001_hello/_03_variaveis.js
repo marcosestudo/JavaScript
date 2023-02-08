b84a0c5c0b24c91f2ca14022a602952d842fc8a7
Marcos Fernando <estudogit@outlook.com>
2023-02-08 13:03:29 -0300

    variaveis

---
+++ _001_hello/_03_variaveis.js
@@ -0,0 +1,7 @@
+// let e var são formas de declarar variáveis, let vem de "let it change"
+var nome;
+nome = "Marcos";
+
+let nome2 = "Fernando"; // let só pode ser modificado internamemte na função em que foi declarada
+
+const num = 53;
