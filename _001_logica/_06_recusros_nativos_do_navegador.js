3ba4600d3d81a5f4f87c34572fe0f43bc0f003e9
Marcos Fernando <estudogit@outlook.com>
2023-02-09 12:02:02 -0300

    recursos nativos do navegador

---
+++ _001_logica/_06_recusros_nativos_do_navegador.js
@@ -0,0 +1,17 @@
+let nome = "Marcos";
+
+// console
+console.log(nome);
+console.error(nome);
+console.warn(nome);
+
+// window 
+// window é o objeto global do navegador, podemos usar todas as suas funções sem digitar o window
+// lembre-se de digitar window pra saber quais são as funções disoníveis
+// window.alert("Alerta 1: " + nome);
+// alert("Alerta 2: " + nome);
+// window.prompt("Digite algo");
+// window.confirm("Sim ou Não");
+
+// document
+// manipula os elemnetos da página
