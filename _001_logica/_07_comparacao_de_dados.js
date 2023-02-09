4f0d49ccb5745dde2fe916d485fa57dd27b91384
Marcos Fernando <estudogit@outlook.com>
2023-02-09 12:33:52 -0300

    comparacao de dados

---
+++ _001_logica/_07_comparacao_de_dados.js
@@ -0,0 +1,5 @@
+console.log(3 == "3"); // comparação de valor
+console.log(3 === "3"); // comparação de valor e tipo
+
+console.log(3 != "3"); // comparação de valor
+console.log(3 !== "3"); // comparação de valor e tipo
