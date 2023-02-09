e8caa7e9bc518e5bd78d6191ad02675df8e8d3d5
Marcos Fernando <estudogit@outlook.com>
2023-02-08 23:56:22 -0300

    operadores

---
+++ _001_logica/_04_operadores.js
@@ -0,0 +1,18 @@
+let num = 0;
+console.log(num);
+
+num += 5; // num = num + 5
+console.log(num);
+
+num -= 2;
+console.log(num);
+
+num = 10 % 3; // resto da divisão
+console.log(num);
+
+num++; // incremento
+console.log(num);
+
+let nome = "Marcos";
+nome += " Fernando"; // concatenação de string
+console.log(nome);
