49e3cbc366fc3e9ab4e7ea874a569fe6d5f75bb1
Marcos Fernando <estudogit@outlook.com>
2023-02-13 14:43:26 -0300

    exemplo concat

--- _001_logica/_22_concat.js
+++ _001_logica/_22_concat.js
@@ -1,6 +1,7 @@
 let arr1 = [1, 2, 3];
 let arr2 = ["um", "dois", "três"];
+let arr3 = [10, 20, 30];
 
-let arr3 = arr1.concat(arr2);
+let arr4 = arr1.concat(arr2, arr3);
 
-console.log(arr3);
+console.log(arr4);
