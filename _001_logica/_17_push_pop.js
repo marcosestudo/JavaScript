fc266700df54072366a25001f9c386439ef9a692
Marcos Fernando <estudogit@outlook.com>
2023-02-13 09:12:59 -0300

    push pop

---
+++ _001_logica/_17_push_pop.js
@@ -0,0 +1,22 @@
+// push() insere elemento no final do array
+// útil parar iserir elemento sem precisar saber o tamanho do array
+let arr = [];
+console.log(arr);
+
+arr.push(1);
+console.log(arr);
+
+
+arr.push(10);
+console.log(arr);
+
+arr.push(100);
+console.log(arr);
+
+
+// pop() remove e retorna o último elemento do array
+console.log(arr.pop());
+console.log(arr);
+
+console.log(arr.pop());
+console.log(arr);
