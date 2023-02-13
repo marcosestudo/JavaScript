06e8e6f763388678b7d70bb3f1a11265fbd43e32
Marcos Fernando <estudogit@outlook.com>
2023-02-13 09:31:55 -0300

    indexOf

---
+++ _001_logica/_20_indexOf.js
@@ -0,0 +1,6 @@
+// indexOf() retorna o indice ou -1 se o elemento não existir
+let arr = [0, 1, 2, 3, "quatro"];
+
+console.log(arr.indexOf(1));
+console.log(arr.indexOf("quatro"));
+console.log(arr.indexOf("1")); // faz checagem de valor e tipo
