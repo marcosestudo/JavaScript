15001f4b91b62071de8eb91ae81c771311430d0f
Marcos Fernando <estudogit@outlook.com>
2023-02-13 09:18:18 -0300

    shift unshift

---
+++ _001_logica/_18_shift_unshift.js
@@ -0,0 +1,15 @@
+let arr = [0,1 ,2];
+
+// unshift() adiciona elemento no início e retorna novo tamanho do array
+console.log(arr.unshift(10));
+console.log(arr);
+
+console.log(arr.unshift(100));
+console.log(arr);
+
+// shift remove e retorna primeiro elemento do array
+console.log(arr.shift());
+console.log(arr);
+
+console.log(arr.shift());
+console.log(arr);
