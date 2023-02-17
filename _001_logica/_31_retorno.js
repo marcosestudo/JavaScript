19530ba14d7cb19b354e7d4b9630a55a6fff4eaa
Marcos Fernando <estudogit@outlook.com>
2023-02-17 10:22:12 -0300

    retornos multiplos

--- _001_logica/_31_retorno.js
+++ _001_logica/_31_retorno.js
@@ -5,9 +5,7 @@
 function calcularMedia(a, b) {
   const media = (a + b) / 2;
 }
-
 const resultado = calcularMedia(5, 9);
-
 console.log(resultado);
 
 
@@ -17,9 +15,7 @@ function criarProduto(nome, preco) {
   const produto = { nome, preco, estoque: 1 }
   return produto;
 }
-
 const notebook = criarProduto("TV", 3000);
-
 console.log(notebook);
 
 
@@ -40,3 +36,29 @@ function areaQuadrada(lado) {
   return areaRetangular(lado, lado);
 }
 console.log(areaQuadrada(8));
+
+
+
+// Uma função só pode ter uma saída, depois do return nada é executado
+function olaMundo() {
+  let texto = "...";
+  return texto;
+  // Repare que o VS Code nos avisa que nosso código não será executado
+  texto = "Olá, mundo!";
+  console.log(texto);
+}
+console.log(olaMundo());
+
+
+
+// No entanto, diferentes ramificações dentro
+// da função podem ter diferentes retornos
+function maioridade(idade) {
+  if (idade >= 18) {
+    return "Maior de idade"
+  } else {
+    return "Menor de idade"
+  }
+}
+console.log(maioridade(20))
+console.log(maioridade(13))
