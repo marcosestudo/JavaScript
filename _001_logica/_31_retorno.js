23179c828ba5ff7c558b05bde7dbc964167fea05
Marcos Fernando <estudogit@outlook.com>
2023-02-17 10:18:20 -0300

    retorno em retorno

--- _001_logica/_31_retorno.js
+++ _001_logica/_31_retorno.js
@@ -3,7 +3,7 @@
 // O retorno é usado para armazenarmos a saída da função em uma variável
 // Se não explicitarmos qual deve ser a saída da função, sua saída será undefined
 function calcularMedia(a, b) {
-  const media = (a + b) / 2
+  const media = (a + b) / 2;
 }
 
 const resultado = calcularMedia(5, 9);
@@ -15,7 +15,7 @@ console.log(resultado);
 // Para dizer qual é a saída de uma função usamos o return
 function criarProduto(nome, preco) {
   const produto = { nome, preco, estoque: 1 }
-  return produto
+  return produto;
 }
 
 const notebook = criarProduto("TV", 3000);
@@ -24,6 +24,19 @@ console.log(notebook);
 
 
 
-
 // Também podemos jogar a saída de uma função dentro dos parâmetros de outra
 console.log(criarProduto("Celular", 1000));
+
+
+
+// E o retorno não precisa ser uma variável,
+// pode ser o resultado de qualquer expressão
+function areaRetangular(base, altura) {
+  return base * altura;
+}
+console.log(areaRetangular(3, 5));
+// Ou até o retorno de outra função
+function areaQuadrada(lado) {
+  return areaRetangular(lado, lado);
+}
+console.log(areaQuadrada(8));
