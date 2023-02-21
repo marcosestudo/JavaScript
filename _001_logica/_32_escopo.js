579ac876e732bb06706f02751cddfb53b8bb6f36
Marcos Fernando <estudogit@outlook.com>
2023-02-21 16:10:08 -0300

    escopo

---
+++ _001_logica/_32_escopo.js
@@ -0,0 +1,23 @@
+// Escopo é o contexto onde a variável foi declarada podendo ser
+// mais externo, ou mais interno (se estiver dentro de um bloco)
+
+// Variáveis no escopo mais externo podem ser
+// usadas dentro de escopos mais internos
+let pokemon = "Charmander"
+
+function evoluir() {
+  pokemon = "Charmeleon"
+}
+
+console.log(pokemon)
+evoluir()
+console.log(pokemon)
+
+// Variáveis no escopo mais interno não podem ser
+// usadas fora dele em escopos mais internos
+function criarAnimal() {
+  let animal = "Gato"
+}
+
+criarAnimal()
+// console.log(animal) // Gera erro
