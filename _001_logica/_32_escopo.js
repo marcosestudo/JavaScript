e7bdc227e85c40d79776df04a2f736e2133663ba
Marcos Fernando <estudogit@outlook.com>
2023-02-21 16:24:04 -0300

    let var

--- _001_logica/_32_escopo.js
+++ _001_logica/_32_escopo.js
@@ -3,21 +3,47 @@
 
 // Variáveis no escopo mais externo podem ser
 // usadas dentro de escopos mais internos
-let pokemon = "Charmander"
+let pokemon = "Charmander";
 
 function evoluir() {
-  pokemon = "Charmeleon"
+  pokemon = "Charmeleon";
 }
 
-console.log(pokemon)
-evoluir()
-console.log(pokemon)
+console.log(pokemon);
+evoluir();
+console.log(pokemon);
 
 // Variáveis no escopo mais interno não podem ser
 // usadas fora dele em escopos mais internos
 function criarAnimal() {
-  let animal = "Gato"
+  let animal = "Gato";
 }
 
-criarAnimal()
+criarAnimal();
 // console.log(animal) // Gera erro
+
+
+
+// Apenas variáveis declaradas com var ficam
+// disponíveis em um escopo mais externo
+function avaliarNota(nota) {
+  if (nota > 60) {
+    var aprovado = true;
+    let situacao = "Aprovado";
+  } else {
+    var aprovado = false;
+    let situacao = "Reprovado";
+  }
+
+  console.log(nota);
+  console.log(aprovado);
+  // console.log(situacao) // Gera erro
+}
+avaliarNota(83);
+avaliarNota(49);
+
+// Mas mesmo o var não consegue "sair" para fora do escopo da função
+function ola() {
+  var texto = "Olá, mundo!";
+}
+// console.log(texto) // Gera erro
