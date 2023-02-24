e3551ee564ad32c9923c2511001e51cc27c01238
Marcos Fernando <estudogit@outlook.com>
2023-02-24 15:45:32 -0300

    segundo exemplo

--- _001_logica/_34_recursao.js
+++ _001_logica/_34_recursao.js
@@ -1,12 +1,21 @@
 // uma função recursiva é uma função que chama ela mesma
-
-let num = 0;
-
 function recursiva(inicio) {
   if (inicio < 5) {
     console.log(inicio);
     recursiva(inicio + 1);
   }
 }
+// recursiva(0);
 
-recursiva(num);
+
+
+// mais um exemplo de função recursiva
+function dividir(num) {
+  console.log(num);
+  if (num % 2 === 0) { // se for par, divide de novo por 2
+    dividir(num / 2);
+  } else {
+    return num;
+  }
+}
+dividir(40);
