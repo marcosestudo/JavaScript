7ea701eb20a7a912874e2ae09228ba9ce22118dd
marcos <estudogit@outlook.com>
2023-03-04 19:25:36 -0300

    objetos globais

---
+++ _001_logica/_41_objetos_globais.js
@@ -0,0 +1,9 @@
+// São objetos padrão da linguagem javascript que ficam sempre disponíveis para utilizarmos.
+// Não confundir com o objeto de escopo global window do navegador (ou o escopo global de outros ambientes)
+// Exs:
+Object;
+Array; 
+String; 
+Number; 
+Date; 
+Math;
