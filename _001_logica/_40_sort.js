0b21134af7f8eccd47524c69fcbbd66c3c695894
marcos <estudogit@outlook.com>
2023-03-04 18:08:06 -0300

    mantendo array original sem modificacao

--- _001_logica/_40_sort.js
+++ _001_logica/_40_sort.js
@@ -12,6 +12,20 @@ const personagens = [
 // Ordena os elementos de um array a partir de comparações entre duplas de elementos
 // CUIDADO: o sort modifica o array original
 personagens.sort(function (a, b) {
-  return a.nivel - b.nivel;
+  return a.nivel - b.nivel; // compara os níveis dois a dois para ordenar
 });
+personagens.forEach((personagem) => console.log(personagem.nivel));
+
+
+
+// Como ordenar sem mudar o array original?
+// O slice cria uma cópia do array com o intervalo que pedimos, encadeando
+// o slice sem passar nenhum parãmetro(para que todo o array seja copiado)
+// ordenaremos a cópia, deixando o array original sem modificação
+let personagens2 = personagens.slice().sort(function (a, b) {
+  return b.nivel - a.nivel; // Aqui ordenaremos em ordem decrescente
+});
+console.log("---------------");
+personagens2.forEach((personagem) => console.log(personagem.nivel));
+console.log("---------------");
 personagens.forEach((personagem) => console.log(personagem.nivel));
