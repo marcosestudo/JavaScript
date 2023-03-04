0392b677ee9c10660bfa8742e6b0628988329961
marcos <estudogit@outlook.com>
2023-03-04 12:58:33 -0300

    referencia vs execucao

--- _001_logica/_36_high_order_funcition.js
+++ _001_logica/_36_high_order_funcition.js
@@ -1,7 +1,21 @@
+// High order functions
 // Funções de alta-ordem, ou high-order functions, são funções que
-// recebem outras funções como parâmetro e as executam em algum momento
+// recebem outras funções como parâmetro ou retornam uma função
 function calcular(a, b, operacao) {
-  console.log("Realizando uma operação.")
-  const resultado = operacao(a, b)
-  return resultado
+  console.log("Realizando uma operação.");
+  const resultado = operacao(a, b);
+  return resultado;
 }
+
+
+
+// Referenciar função vs chamar / executar função
+// Repare que não usamos os () ao lado do nome da função,
+// porque ao fazer isso estaríamos executando a função
+function somar(x, y) {
+  console.log("Realizando soma.")
+  return x + y;
+}
+console.log(calcular(3, 5, somar));
+console.log(somar);      // Printa a própria função
+console.log(somar(1, 1)); // Chama a função (printa o retorno da função)
