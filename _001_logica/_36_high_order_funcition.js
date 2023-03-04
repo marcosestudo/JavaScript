239486e22b8b21e2d6d281b11ea4cc53cf428d5d
marcos <estudogit@outlook.com>
2023-03-04 13:21:37 -0300

    callback

--- _001_logica/_36_high_order_funcition.js
+++ _001_logica/_36_high_order_funcition.js
@@ -25,6 +25,34 @@ console.log(somar(1, 1)); // Chama a função (printa o retorno da função)
 // Função anõnima como parâmetro
 // Também podemos escrever funções anônimas dentro da própria chamada da high-order function
 calcular(9, 2, function (x, y) {
-  console.log("Realizando substração.")
-  return x - y
-})
+  console.log("Realizando substração.");
+  return x - y;
+});
+
+
+// Callbacks
+// Essas funções que são passadas como parâmetros geralmente são chamadas de callbacks
+// Um exemplo comum de high-order function no javascript é a função .forEach() dos arrays
+function exibirElemento(elemento, indice, array) {
+  console.log({
+    elemento,
+    indice,
+    array
+  });
+}
+
+const lista = ["Maçã", "Banana", "Laranja", "Limão", "Uva"];
+// Forma tradicional
+for (let i = 0; i < lista.length; i++) {
+  exibirElemento(lista[i], i, lista);
+}
+// Forma funcional
+lista.forEach(exibirElemento);
+// Também poderia ser feito:
+lista.forEach(function (elemento, indice, array) {
+  console.log({
+    elemento,
+    indice,
+    array
+  });
+});
